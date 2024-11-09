0d0e27802bdfef8fc18bb9d38d30b0024ee67b68
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-09 22:25:24 +0100

    Add toggle button without functionality

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import recipes from "../data/recipesById.json";
+import HeartFilledIcon from "../assets/images/HeartFilledIcon";
+import HeartOutlineIcon from "../assets/images/HeartOutlineIcon";
 
 export function RecipeCard() {
+  const [liked, setLiked] = useState(false);
+
+  const toggleLike = () => {
+    setLiked(!liked);
+  };
+
   const recipe = recipes.map((recipe) => (
     <div key={recipe.id} className="card">
       <h3>{recipe.title}</h3>
       <img src={recipe.image} alt={recipe.title} className="recipe-image" />
+      <button onClick={toggleLike} className="like-button">
+        {liked ? (
+          <HeartFilledIcon />
+        ) : (
+          <HeartOutlineIcon/>
+        )}
+      </button>
     </div>
   ));
 
