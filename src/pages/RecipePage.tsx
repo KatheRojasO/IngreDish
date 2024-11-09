3e1575c4a88340799758ea9c6f3474fea7e90fe7
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-09 10:32:22 +0100

    Create recipe page

---
+++ src/pages/RecipePage.tsx
@@ -0,0 +1,16 @@
+import { SignedIn, UserButton } from "@clerk/clerk-react";
+import { Recipes } from "../components/Recipes";
+import logo from "../assets/logo.png";
+
+export function RecipePage() {
+  return (
+    <SignedIn>
+      <header className="recipe_page_header">
+        <img src={logo} alt="logo" className="logo" />
+        <UserButton />
+      </header>
+
+      <Recipes />
+    </SignedIn>
+  );
+}
