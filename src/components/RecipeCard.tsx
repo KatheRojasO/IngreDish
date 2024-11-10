23194a7a5929c70ddac1d090c76033bbad4c07c9
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-10 12:02:38 +0100

    Refactor code to make like icon works

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,24 +1,24 @@
 import { useState } from "react";
-import recipes from "../data/recipesById.json";
+import { RecipeCardProps } from "../types/Recipe";
 import HeartFilledIcon from "../assets/images/HeartFilledIcon";
 import HeartOutlineIcon from "../assets/images/HeartOutlineIcon";
 
-export function RecipeCard() {
+export function RecipeCard({recipeInfo}: RecipeCardProps) {
   const [liked, setLiked] = useState(false);
 
   const toggleLike = () => {
     setLiked(!liked);
   };
 
-  const recipe = recipes.map((recipe) => (
-    <div key={recipe.id} className="card">
-      <h3>{recipe.title}</h3>
-      <img src={recipe.image} alt={recipe.title} className="recipe-image" />
-      <button onClick={toggleLike} className="like-button">
-        {liked ? <HeartFilledIcon /> : <HeartOutlineIcon />}
-      </button>
+  return (
+    <div className="card-container">
+      <div className="card">
+        <h3>{recipeInfo.title}</h3>
+        <img src={recipeInfo.image} alt={recipeInfo.title} className="recipe-image" />
+        <button onClick={toggleLike} className="like-button">
+          {liked ? <HeartFilledIcon /> : <HeartOutlineIcon />}
+        </button>
+      </div>
     </div>
-  ));
-
-  return <div className="card-container">{recipe}</div>;
+  );
 }
--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -1,10 +1,15 @@
-import { RecipeCard } from './RecipeCard';
+import { RecipeCard } from "./RecipeCard";
+import { RecipesProps } from "../types/Recipe";
 
-export function Recipes() {
+export function Recipes({ recipesInfo }: RecipesProps) {
+  const recipes = recipesInfo.map((recipe) => {
+    return <RecipeCard key={recipe.id} recipeInfo={recipe} />;
+  });
+
   return (
-    <div className='recipes_container'>
+    <div className="recipes_container">
       <h1>Recipes</h1>
-      <RecipeCard />
+      {recipes}
     </div>
-  )
+  );
 }
