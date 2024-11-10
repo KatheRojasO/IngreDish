3bb0d399130dffd95b23e174c7707ab77ab68612
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-10 13:53:24 +0100

    Initialize backend

---
+++ backend/src/main/java/org/example/ingredish/IngredishApplication.java
@@ -0,0 +1,13 @@
+package org.example.ingredish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngredishApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/example/ingredish/IngredishApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ingredish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
