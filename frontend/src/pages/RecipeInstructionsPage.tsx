af7730da538f5c85b062054fccc040f77f102be0
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-11 17:59:31 +0100

    Style recipe instructions page

--- frontend/src/pages/RecipeInstructionsPage.tsx
+++ frontend/src/pages/RecipeInstructionsPage.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import { Header } from '../components/Header'
+import { Header } from "../components/Header";
+import data from "../data/recipeInstructions.json";
 
 export default function RecipeInstructionsPage() {
+  const image = data.image;
+  const steps = data.analyzedInstructions.flatMap((instruction) =>
+    instruction.steps.map((step) => step.step)
+  );
+
   return (
-    <div>
+    <div className="recipe-instructions-container">
       <Header />
-      <p>We're working to bring you the best! Stay tune!</p>
+      <div className="blabla">
+        <div className="instructions-card">
+          <h3>{data.title}</h3>
+          <img src={image} alt={data.title} className="instructions-image" />
+          <p>{steps}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
