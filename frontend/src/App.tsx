3041939587dc32b3dd4b8c782b29bc661404be9a
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-11 16:02:27 +0100

    Add styling for welcome page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,21 @@
-import { SignedOut, SignInButton } from "@clerk/clerk-react";
 import { RecipePage } from "./pages/RecipePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FavoritesPage from "./pages/FavoritesPage";
 import RecipeInstructionsPage from "./pages/RecipeInstructionsPage";
+import MainPage from "./pages/MainPage";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
+        <MainPage />
         <Routes>
           <Route path="/" element={<RecipePage />} />
           <Route path="/favorites" element={<FavoritesPage />} />
-          <Route path="/recipeDescription" element={<RecipeInstructionsPage />} />
+          <Route
+            path="/recipeDescription"
+            element={<RecipeInstructionsPage />}
+          />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/pages/MainPage.tsx
@@ -0,0 +1,18 @@
+import WelcomePageHeader from "../components/WelcomePageHeader";
+import welcomeImage from "../assets/welcome-image.png";
+
+export default function MainPage() {
+  return (
+    <div className="main-page">
+      <WelcomePageHeader />
+      <div className="main-page-container">
+        <div className="text-block">
+          <h1 className="welcome-title">Welcome to the Ingredish!</h1>
+          <p className="welcome-description">
+            The best place to find recipes for your favorite ingredients!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
