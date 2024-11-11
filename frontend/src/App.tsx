669791e182005505c0f545297c8886051813474d
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-11 16:41:26 +0100

    Refactor to make login page dissapear from recipes page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FavoritesPage from "./pages/FavoritesPage";
 import RecipeInstructionsPage from "./pages/RecipeInstructionsPage";
 import MainPage from "./pages/MainPage";
+import { SignedOut } from "@clerk/clerk-react";
+import WelcomePageHeader from "./components/WelcomePageHeader";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        <MainPage />
+        <SignedOut>
+          <WelcomePageHeader />
+          <MainPage />
+        </SignedOut>
+
         <Routes>
           <Route path="/" element={<RecipePage />} />
           <Route path="/favorites" element={<FavoritesPage />} />
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,10 +1,6 @@
-import WelcomePageHeader from "../components/WelcomePageHeader";
-import welcomeImage from "../assets/welcome-image.png";
-
 export default function MainPage() {
   return (
     <div className="main-page">
-      <WelcomePageHeader />
       <div className="main-page-container">
         <div className="text-block">
           <h1 className="welcome-title">Welcome to the Ingredish!</h1>
